b13b5935d33cdab1770ecf5938c9d0e46df9a9a5
marjorystipp <163565166+marjorystipp@users.noreply.github.com>
2025-07-30 13:37:19 -0300

    script

---
+++ script.js
@@ -0,0 +1,6 @@
+function inserirNome(){
+    let nomeUsuario = prompt("Qual o seu nome");
+    let elemento = document.querySelector("#nome-usuario");
+    console.log(elemento);
+    elemento.textContent = nomeUsuario;
+}
