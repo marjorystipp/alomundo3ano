e891cab335c33f6f10a40c01aa2fb4e916ed4db0
marjorystipp <163565166+marjorystipp@users.noreply.github.com>
2025-08-01 15:13:28 -0300

    matéria

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function inserirNome(){
     console.log(elemento);
     elemento.textContent = nomeUsuario;
 }
+
+const lista = document.querySelector('#lista');
+
+lista.textContent = linguagens[0];
+lista.textContent = linguagens[1];
+lista.textContent = linguagens[2];
+
+let aluno1 = {
+    nome: 'marjory',
+    idade: 17,
+    anoLetivo: 'Cursando o ensino médio',
+    materiasfavoritas:['matematica II','Biologia','Ed física']
+}
